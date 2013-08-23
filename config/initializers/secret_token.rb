c14336da5b6c5e1ae06ac2ad235f5b5313bb3a93
nakamura shinichirou <naka5313@gmail.com>
2013-08-24 02:10:52 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LifePhotos::Application.config.secret_key_base = '992e872de0b6973f90e653075f978484084a2eb595c130b67c9dc434d0fa24ccaf87dfad33daab13d8f96acea56f8624e72cd612d21fddd5f8f1797f86fdee8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LifePhotos::Application.config.session_store :cookie_store, key: '_life_photos_session'
