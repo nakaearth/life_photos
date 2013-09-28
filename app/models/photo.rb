18ceb851192c18e8f518eeab1c084c0700e46866
nakamura shinichirou <naka5313@gmail.com>
2013-09-29 00:18:08 +0900

    update files

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ class Photo < ActiveRecord::Base
   validates :title,  presence: true
 
   Paperclip.interpolates :img_dir_num do |attachment, style|
-    (attachment.instance.id * 0.01).to_i
+    (attachment.instance.user_id).to_s + "/" + (attachment.instance.id * 0.01).to_s 
   end 
 
   S3_CREDENTIALS = { access_key_id: ENV['S3_ACCESS_KEY_ID'], secret_access_key: ENV['S3_SECRET_KEY'], bucket: "lifePhoto-bucket" }
@@ -17,7 +17,7 @@ class Photo < ActiveRecord::Base
       path: "photos/:img_dir_num/:id/:style/:filename"
   else 
     has_attached_file :photo,
-      url: "/system/img/attaches/:id/:style/:filename" ,
+      url: "/system/img/attaches/:img_dir_num/:id/:style/:filename" ,
       styles:  { medium:  "350x350>", thumb:  "100x100>", original: "700x700>" }
   end
 
