93dd8289a73eeaa7d3b64056aaab949a27701843
nakamura shinichirou <naka5313@gmail.com>
2014-03-09 23:20:32 +0900

    update photo_comment.rb

--- app/models/photo_comment.rb
+++ app/models/photo_comment.rb
@@ -1,7 +1,6 @@
 class PhotoComment < ActiveRecord::Base
-  belongs_to: photo, dependent: destroy
-  belongs_to: user, dependent: destroy
+  belongs_to :photo, dependent: :destroy
+  belongs_to :user, dependent: :destroy
 
   validates :message, presence: true
-
 end
