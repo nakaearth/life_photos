4be08a4bb404caf8cddfd512cd82b05c0d0947f9
nakamura shinichirou <naka5313@gmail.com>
2014-03-10 00:14:25 +0900

    update files

--- app/models/photo_comment.rb
+++ app/models/photo_comment.rb
@@ -1,6 +1,5 @@
 class PhotoComment < ActiveRecord::Base
-  belongs_to :photo, dependent: :destroy
-  belongs_to :user, dependent: :destroy
-
+  belongs_to :photo
+  belongs_to :user
   validates :message, presence: true
 end
