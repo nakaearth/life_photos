71d01389f79d389b20fb9c533b764350b1184f9f
nakamura shinichirou <naka5313@gmail.com>
2014-03-26 00:50:27 +0900

    rspec file update

--- spec/models/photo_comment_spec.rb
+++ spec/models/photo_comment_spec.rb
@@ -7,14 +7,17 @@ describe PhotoComment do
   let!(:photo1) { FactoryGirl.create(:current_user_photo1, user: test_user1, album: test_album) }
   #let!(:test_photo_comment1) { FactoryGirl.create(:photo_comment1, user: test_user1, photo: photo1) }
 
-  describe "関連をテスト" do
+  describe "関連やバリデーションをテスト" do
 #    it { expect belong_to(:photo) }
 #    it { expect belong_to(:user) }
+    it { expect have_db_column(:message) }
+    it { expect have_db_column(:photo_id) }
+    it { expect validate_presence_of(:message) }
   end
   
   context "コメント投稿" do
     before do
-      
+        
     end
 
     it "normal case" do
