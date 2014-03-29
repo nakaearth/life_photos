e1bbff0f10ea5e135900dad862cad214787be5da
nakamura shinichirou <naka5313@gmail.com>
2014-03-30 00:09:22 +0900

    update spec files

--- app/controllers/lifephoto/comment_controller.rb
+++ app/controllers/lifephoto/comment_controller.rb
@@ -1,5 +1,6 @@
 class Lifephoto::CommentController < ApplicationController
   def index
+    p params[:photo_id]
     @photo = Photo.find(params[:photo_id])
     @photo_comments = @photo.photo_comments
     @comment = PhotoComment.new
